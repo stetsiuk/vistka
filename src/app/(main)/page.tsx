4ba2aca647a58d3330aa8e30c35f705714f74072
Oleksandr Stetsiuk <olexander.stetsyuk@gmail.com>
2024-10-06 16:27:00 +0300

    Posts page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { postDataInclude } from "@/lib/types";
+import { PostEditor } from "@/components/posts/editor/PostEditor";
+import { Post } from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">front page</div>
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/lib/types.ts
@@ -0,0 +1,17 @@
+import { Prisma } from "@prisma/client";
+
+export const postDataInclude = {
+  user: {
+    select: {
+      username: true,
+      displayName: true,
+      avatarUrl: true,
+    },
+  },
+} satisfies Prisma.PostInclude;
+
+export type PostData = Prisma.PostGetPayload<{
+  include: {
+    user: { select: { username: true; displayName: true; avatarUrl: true } };
+  };
+}>;
